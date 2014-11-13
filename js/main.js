6c72ca94f66c533e503ed459d6a3a00f635866cf
susanna3679 <yueyuesong@Yueyues-MacBook-Pro.local>
2014-11-13 16:49:01 -0500

    Finished Boilerplate - 11/13

---
+++ js/main.js
@@ -0,0 +1,59 @@
+//Equal Height Initialize
+$(window).load(function() {
+    equalheight('.equal-height');
+});
+
+$(window).resize(function(){
+    equalheight('.equal-height');
+});
+
+
+// Fixed Header Height Offset Initialize
+//$(document).ready(function() {
+//    header_height = $(".no-height").height();
+//    $("main").css("padding-top", header_height);
+//});
+//
+//$(window).resize(function() {
+//    header_height = $(".no-height").height();
+//    $("main").css("padding-top", header_height);
+//});
+
+
+// Responsive Nav Initialize
+//$(document).ready(function() {
+//	$('body').addClass('js');
+//	var $menu = $('#menu'),
+//		$menulink = $('.menu-link'),
+//        $menuwrap = $('.menu-wrap');
+//	
+//	$menulink.click(function() {
+//        $menuwrap.toggleClass('menu-wrap-styles');
+//		$menulink.toggleClass('active');
+//		$menu.toggleClass('active');
+//		return false;
+//	});
+//});
+
+
+// Animated Page Scroll Initialize
+//jQuery(document).ready(function($) {
+//    $(".scroll").click(function(event){		
+//        event.preventDefault();
+//        $('html,body').animate({scrollTop:$(this.hash).offset().top}, 800);
+//    });
+//});
+
+
+// bigtext.js Initialize
+//$(function() {
+//    WebFont.load({
+//        custom: {
+//            families: ['Yanone Kaffeesatz'], // font-family name
+//            urls : ['css/main.css'] // URL to css
+//        },
+//        active: function() {
+//            $('#bigtext').bigtext();
+//        }
+//    });
+//});              
---
+++ readme.md
@@ -0,0 +1,11 @@
+# Project & Demo Boilerplate/Framework
+As you start a new demo or project you should use this boilerplate/framework as a beginning point. As you learn more throughout the semester new features will be added to the boilerplate/framework.
+
+## Resources
+### Javascript & jQuery Goodies
+- <a href="http://jxnblk.com/fitter-happier-text/" target="_blank" title="Fitter Happier Text">Fitter Happier Text</a>
+
+### Responsive Frameworks
+- <a href="http://getbootstrap.com/" target="_blank" title="Boostrap">Boostrap</a>
+- <a href="http://foundation.zurb.com/" target="_blank" title="Zurb Foundation">Zurb Foundation</a>
+- <a href="http://gumbyframework.com/" target="_blank" title="Gumby">Gumby</a>
